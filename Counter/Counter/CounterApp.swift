b6f7b1c269b7ad5fdb7749ac9fa363abd1f6724e
Theo Alexiou <theofilosalexiou@gmail.com>
2021-03-17 19:43:16 +0100

    Initial xcode stuff

---
+++ Counter/Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Theofilos Alexiou on 17/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
